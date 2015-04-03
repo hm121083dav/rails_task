bd98eb88480c0aa192858bde92af6c84b5fe17c1
duxa <hm121083dav@gmail.com>
2015-04-03 15:00:45 +0300

    Удаление ненужных файлов

--- db/migrate/20150322102309_status.rb
+++
@@ -1,7 +0,0 @@
-class Status < ActiveRecord::Migration
-  def change
-    create_table :status do |t|
-      t.string :title
-    end
-  end
-end
--- db/migrate/20150322102412_teams.rb
+++
@@ -1,8 +0,0 @@
-class Teams < ActiveRecord::Migration
-  def change
-    create_table :teams do |t|
-      t.string :title
-      t.string :short_title
-    end
-  end
-end
