6d8d8be6f796934163ba451d9184e3ba98ed58b9
duxa <hm121083dav@gmail.com>
2015-04-05 15:20:50 +0300

    Модифицировал заготовку для контроллера тиккета

--- app/controllers/admin/tickets_controller.rb
+++ app/controllers/admin/tickets_controller.rb
@@ -1,26 +1,32 @@
 class Admin::TicketsController < ApplicationController
 
+  #Вывести все заявки и их текущее состояние
   def work_and_progress_ticket
 
   end
 
+  #Прееназначить заявку другому сотруднику
   def reassign_ticket
 
   end
 
+  #Взять в заявку в работу
   def start_working_ticket
 
   end
 
+  #Закрвытие заявки
   def finished_ticket
 
   end
 
+  #Архив заявок
   def show_history_ticket
 
   end
 
+  #Послать электронное сообщение пользователю
   def send_question_to_requester
-  
+
   end
 end
