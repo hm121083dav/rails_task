21e66f1f7fb7ed8f48b6af6b43dea40b9336ad19
duxa <hm121083dav@gmail.com>
2015-04-05 18:27:56 +0300

    Сделал заготовку контроллера для журнала заявок

---
+++ app/controllers/admin/logs_controller.rb
@@ -0,0 +1,15 @@
+class Admin::LogsController < ApplicationController
+
+  #Добавление записи в журнал
+  def create
+
+  end
+
+  private
+
+  #Обработчик параметров с формы для записи в журнал
+  def log_params
+
+  end
+
+end
