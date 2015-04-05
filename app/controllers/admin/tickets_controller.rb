02319c6fa91fd3738d3f05e8d4c4af4716ab888d
duxa <hm121083dav@gmail.com>
2015-04-05 15:01:59 +0300

    Создал заготовку для контроллера который будет работать с тиккетами (менять статус и тп)

---
+++ app/controllers/admin/tickets_controller.rb
@@ -0,0 +1,26 @@
+class Admin::TicketsController < ApplicationController
+
+  def work_and_progress_ticket
+
+  end
+
+  def reassign_ticket
+
+  end
+
+  def start_working_ticket
+
+  end
+
+  def finished_ticket
+
+  end
+
+  def show_history_ticket
+
+  end
+
+  def send_question_to_requester
+  
+  end
+end
